a7b0383a5bf77c986660eab127ce5179c96bd49a
Velcipher <108995955+Velcipher@users.noreply.github.com>
2022-07-17 21:35:54 -0400

    modal structure changes/adding scrolling

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const toHtml = trip =>`
       <div class="card-body">
         <h5 class="card-title">${trip.tittle}</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a  class="btn" data-btn='go'>Go somewhere</a>
+        <a  class="btn" data-btn='go'>More info</a>
       </div>
         </div>
     </div>
